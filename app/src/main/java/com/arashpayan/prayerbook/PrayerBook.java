aede344988c7e4339a845f2d46ba8325ba6a11f6
Arash Payan <arash@ara.sh>
2015-06-28 19:25:17 -0700

    Uses new design support library
    Instead of using the action bar compat, it now uses the toolbar. Visually, everything is exactly the same, except the toolbar now scrolls up when viewing results in the SearchFragment.

--- app/src/main/java/com/arashpayan/prayerbook/PrayerBook.java
+++ app/src/main/java/com/arashpayan/prayerbook/PrayerBook.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class PrayerBook extends AppCompatActivity {
 
@@ -12,6 +13,8 @@ public class PrayerBook extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.prayer_book);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.pb_toolbar);
+        setSupportActionBar(toolbar);
 
         if (savedInstanceState == null) {
             CategoriesFragment categoriesFragment = new CategoriesFragment();
