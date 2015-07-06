1606e817e2d5f5c72a22636d0da639485244b3c5
Arash Payan <arash@ara.sh>
2015-07-06 11:34:46 -0700

    Fixed language header for pre-v17 devices

--- app/src/main/java/com/arashpayan/prayerbook/CategoriesAdapter.java
+++ app/src/main/java/com/arashpayan/prayerbook/CategoriesAdapter.java
@@ -1,7 +1,6 @@
 package com.arashpayan.prayerbook;
 
 import android.database.Cursor;
-import android.graphics.drawable.RippleDrawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
